c33d50749ffb3a9f21c6ed85a8aaba09e022003b
Linh Pham <bptl1603@gmail.com>
2021-11-17 15:25:40 +0100

    limit player position

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,7 +15,8 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        transform.position = new Vector3(
+                Mathf.Clamp(transform.position.x, -33, 33), transform.position.y, transform.position.z);
     }
 
     void FixedUpdate()
